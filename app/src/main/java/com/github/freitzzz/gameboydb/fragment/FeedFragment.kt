275644ddc3bdad63cef97e5e24037a3d3e349bce
João Freitas <1160907@isep.ipp.pt>
2025-01-14 16:27:23 +0000

    refactor: isolate feed page in fragment

---
+++ app/src/main/java/com/github/freitzzz/gameboydb/fragment/FeedFragment.kt
@@ -0,0 +1,6 @@
+package com.github.freitzzz.gameboydb.fragment
+
+import androidx.fragment.app.Fragment
+import com.github.freitzzz.gameboydb.R
+
+class FeedFragment : Fragment(R.layout.fragment_feed)
