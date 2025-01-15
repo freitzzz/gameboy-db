3dbe6e7fa811aff8c3c3ed31b1f11579ae1fe147
João Freitas <1160907@isep.ipp.pt>
2025-01-15 18:28:00 +0000

    feat: generate parcelize for GameTile

--- app/src/main/java/com/github/freitzzz/gameboydb/data/model/GameTile.kt
+++ app/src/main/java/com/github/freitzzz/gameboydb/data/model/GameTile.kt
@@ -1,7 +1,10 @@
 package com.github.freitzzz.gameboydb.data.model
 
 import android.net.Uri
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 data class GameTile(
     val id: String,
     val title: String,
@@ -9,6 +12,6 @@ data class GameTile(
     val rating: Float,
     val cover: Uri,
     val genres: List<String>,
-    val screenshots: List<String>,
+    val screenshots: List<Uri>,
     val gameplay: String,
-)
+): Parcelable
