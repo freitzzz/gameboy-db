699b815dec247035e5ce20345c43adf4f095c72e
João Freitas <1160907@isep.ipp.pt>
2025-01-15 23:51:19 +0000

    feat: wrap startActivity to reduce code duplication and imply bundling logic

---
+++ app/src/main/java/com/github/freitzzz/gameboydb/navigation/Navigation.kt
@@ -0,0 +1,67 @@
+package com.github.freitzzz.gameboydb.navigation
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import com.github.freitzzz.gameboydb.data.model.GameTile
+import kotlin.reflect.KClass
+
+/**
+ * A list of bundle keys that are known at compile-time for specific types.
+ * This list is used in [toBundle] to automatically create a bundle with a value.
+ */
+val extraKeys = mapOf<KClass<*>, String>(
+    GameTile::class to "game-tile"
+)
+
+/**
+ * Wrapper around [Context.startActivity] for pushing an activity to the top of the backstack.
+ *
+ * If you need to pass data to the activity, use the [data] param. This param can either be of an
+ * arbitrary type (which will be wrapped around using [toBundle]), or a bundle instance.
+ */
+inline fun <reified A : Activity> Context.navigateTo(data: Any? = null) {
+    this.startActivity(
+        Intent(this, A::class.java).apply {
+            when (data) {
+                null -> return@apply
+                is Bundle -> putExtras(data)
+                else -> putExtras(data.toBundle())
+            }
+        }
+    )
+}
+
+/**
+ * Retrieves data passed to an activity using the [Intent] that was passed on [Context.startActivity].
+ *
+ * This method only works if you have launched the activity using [navigateTo] or used [toBundle]
+ * to wrap data in a bundle.
+ */
+inline fun <reified T : Any> Intent.get(): T? {
+    val key = extraKeys[T::class] ?: T::class.java.name
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        return this.getParcelableExtra(key, T::class.java)
+    }
+
+    @Suppress("DEPRECATION")
+    return this.getParcelableExtra(key) as T?
+}
+
+/**
+ * Wraps this value in a bundle. See [extraKey] to learn how the key is selected to reference the value.
+ */
+fun Any.toBundle() = bundleOf(extraKey(this) to this)
+
+/**
+ * Selects a key to wrap data in a bundle. If the data type was registered in [extraKeys], it uses
+ * that key, else it will use the type java class name as the key (e.g., `extraKey("data") => "kotlin.String"`)
+ */
+private fun extraKey(data: Any) = extraKeys[data::class] ?: data::class.java.name
+
+/**
+ * Direct wrapper around [androidx.core.os.bundleOf] so that the function can be easily mocked in unit tests.
+ */
+fun bundleOf(vararg pairs: Pair<String, Any?>) = androidx.core.os.bundleOf(*pairs)
---
+++ app/src/test/java/com/github/freitzzz/gameboydb/navigation/NavigationTest.kt
@@ -0,0 +1,87 @@
+package com.github.freitzzz.gameboydb.navigation
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import com.github.freitzzz.gameboydb.activity.MainActivity
+import com.github.freitzzz.gameboydb.data.model.GameTile
+import io.mockk.every
+import io.mockk.just
+import io.mockk.mockk
+import io.mockk.mockkConstructor
+import io.mockk.mockkStatic
+import io.mockk.runs
+import io.mockk.verify
+import io.mockk.verifyAll
+import org.junit.Test
+
+class NavigationTest {
+
+    init {
+        mockkStatic(::bundleOf)
+        mockkConstructor(Intent::class)
+    }
+
+    @Test
+    fun `toBundle uses known key for putting GameTile in extras`() {
+        val key = "game-tile"
+        val data = mockk<GameTile>()
+        every { bundleOf(key to data) } returns mockk()
+
+        data.toBundle()
+        verify(exactly = 1) { bundleOf(key to data) }
+    }
+
+    @Test
+    fun `toBundle uses resolved java class name if key is not known for input data`() {
+        val key = String::class.java.name
+        val data = "my bundled data"
+
+        every { bundleOf(key to data) } returns mockk()
+
+        data.toBundle()
+        verify(exactly = 1) { bundleOf(key to data) }
+    }
+
+    @Test
+    fun `navigateTo calls startActivity and doesn't apply a bundle if data is not passed`() {
+        val context = mockk<Context>()
+
+        every { context.startActivity(any()) } just runs
+
+        context.navigateTo<MainActivity>()
+        verify(exactly = 1) { context.startActivity(any()) }
+    }
+
+    @Test
+    fun `navigateTo calls startActivity and applies bundle directly if a bundle is passed as data`() {
+        val context = mockk<Context>()
+        val bundle = mockk<Bundle>()
+
+        every { context.startActivity(any()) } just runs
+        every { anyConstructed<Intent>().putExtras(bundle) } returns mockk()
+
+        context.navigateTo<MainActivity>(data = bundle)
+        verifyAll {
+            context.startActivity(any())
+            anyConstructed<Intent>().putExtras(bundle)
+        }
+    }
+
+    @Test
+    fun `navigateTo calls startActivity and applies bundle created from toBundle if a specific type is passed as data`() {
+        val context = mockk<Context>()
+        val data = mockk<GameTile>()
+        val bundle = mockk<Bundle>()
+
+        every { context.startActivity(any()) } just runs
+        every { anyConstructed<Intent>().putExtras(bundle) } returns mockk()
+        every { bundleOf(any()) } returns bundle
+
+        context.navigateTo<MainActivity>(data = data)
+        verifyAll {
+            context.startActivity(any())
+            anyConstructed<Intent>().putExtras(bundle)
+        }
+    }
+}
