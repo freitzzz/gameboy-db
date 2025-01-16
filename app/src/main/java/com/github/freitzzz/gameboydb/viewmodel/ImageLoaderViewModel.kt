0f0ff049f55a51968bc7d089fb34c0c528af79c5
João Freitas <1160907@isep.ipp.pt>
2025-01-16 10:59:46 +0000

    feat: add viewmodel for image loading

---
+++ app/src/main/java/com/github/freitzzz/gameboydb/viewmodel/ImageLoaderViewModel.kt
@@ -0,0 +1,43 @@
+package com.github.freitzzz.gameboydb.viewmodel
+
+import android.net.Uri
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.github.freitzzz.gameboydb.core.vault
+import com.github.freitzzz.gameboydb.domain.DownloadImage
+import kotlinx.coroutines.launch
+
+class ImageLoaderViewModel : ViewModel() {
+    private val downloadImage by lazy { vault().get<DownloadImage>() }
+
+    operator fun invoke(image: Uri): LiveData<Uri> {
+        return MutableLiveData<Uri>().apply {
+            viewModelScope.launch {
+                val result = downloadImage(image.toString())
+                result.each {
+                    postValue(Uri.fromFile(it))
+                }
+            }
+        }
+    }
+
+    operator fun invoke(images: List<Uri>): LiveData<List<Uri>> {
+        val value = images.toMutableList()
+        return MutableLiveData<List<Uri>>().apply {
+            viewModelScope.launch {
+                for (i in value.indices) {
+                    val result = downloadImage(value[i].toString())
+                    result.each {
+                        value[i] = Uri.fromFile(it)
+                        postValue(value)
+                    }
+                }
+            }
+        }
+    }
+
+    fun loaded(vararg uris: Uri) = uris.all { it.scheme == "file" }
+    fun loaded(uris: List<Uri>) = uris.all { it.scheme == "file" }
+}
