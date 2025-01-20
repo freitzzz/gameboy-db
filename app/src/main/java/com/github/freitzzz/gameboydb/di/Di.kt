42f3279d208aaf8b109e47e9577405ba6351b8ac
João Freitas <1160907@isep.ipp.pt>
2025-01-20 16:11:43 +0000

    chore: inject game updates and load game

--- app/src/main/java/com/github/freitzzz/gameboydb/di/Di.kt
+++ app/src/main/java/com/github/freitzzz/gameboydb/di/Di.kt
@@ -5,14 +5,16 @@ import com.github.freitzzz.gameboydb.core.Vault
 import com.github.freitzzz.gameboydb.data.http.SSLBypassNetworkingClient
 import com.github.freitzzz.gameboydb.data.repository.AssetsRepository
 import com.github.freitzzz.gameboydb.data.repository.FakeGamesRepository
-import com.github.freitzzz.gameboydb.data.repository.NetworkingAssetsRepository
 import com.github.freitzzz.gameboydb.data.repository.GamesRepository
+import com.github.freitzzz.gameboydb.data.repository.NetworkingAssetsRepository
 import com.github.freitzzz.gameboydb.domain.DownloadImage
 import com.github.freitzzz.gameboydb.domain.FavoriteGame
 import com.github.freitzzz.gameboydb.domain.GetControversialGames
 import com.github.freitzzz.gameboydb.domain.GetFavoriteGames
 import com.github.freitzzz.gameboydb.domain.GetTopRatedGames
+import com.github.freitzzz.gameboydb.domain.LoadGame
 import com.github.freitzzz.gameboydb.domain.UnfavoriteGame
+import com.github.freitzzz.gameboydb.domain.state.GameUpdates
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 
@@ -38,10 +40,13 @@ private fun registerData(
 private fun registerDomain(
     vault: Vault,
 ) = vault.apply {
+    store(GameUpdates())
+
+    store(DownloadImage(get()))
     store(GetTopRatedGames(get()))
     store(GetControversialGames(get()))
     store(GetFavoriteGames(get()))
-    store(DownloadImage(get()))
-    store(FavoriteGame(get()))
-    store(UnfavoriteGame(get()))
+    store(LoadGame(get()))
+    store(FavoriteGame(get(), get()))
+    store(UnfavoriteGame(get(), get()))
 }
