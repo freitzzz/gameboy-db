155dff5336afa86e2a56e46b1b3d1f91d5808fb9
João Freitas <1160907@isep.ipp.pt>
2025-01-20 16:10:48 +0000

    feat: emit updated game after it has been marked/unmarked as favorite

--- app/src/main/java/com/github/freitzzz/gameboydb/domain/FavoriteGame.kt
+++ app/src/main/java/com/github/freitzzz/gameboydb/domain/FavoriteGame.kt
@@ -2,11 +2,15 @@ package com.github.freitzzz.gameboydb.domain
 
 import com.github.freitzzz.gameboydb.data.model.Game
 import com.github.freitzzz.gameboydb.data.repository.GamesRepository
+import com.github.freitzzz.gameboydb.domain.state.GameUpdates
 
 class FavoriteGame(
     private val repository: GamesRepository,
+    private val state: GameUpdates,
 ) {
-    suspend operator fun invoke(game: Game) = repository.markFavorite(game).map {
-        game.copy(favorite = true)
+    suspend operator fun invoke(game: Game) = repository.markFavorite(game).each {
+        game.copy(favorite = true).also {
+            state.tryEmit(it)
+        }
     }
 }
--- app/src/main/java/com/github/freitzzz/gameboydb/domain/UnfavoriteGame.kt
+++ app/src/main/java/com/github/freitzzz/gameboydb/domain/UnfavoriteGame.kt
@@ -2,11 +2,15 @@ package com.github.freitzzz.gameboydb.domain
 
 import com.github.freitzzz.gameboydb.data.model.Game
 import com.github.freitzzz.gameboydb.data.repository.GamesRepository
+import com.github.freitzzz.gameboydb.domain.state.GameUpdates
 
 class UnfavoriteGame(
     private val repository: GamesRepository,
+    private val state: GameUpdates,
 ) {
-    suspend operator fun invoke(game: Game) = repository.unmarkFavorite(game).map {
-        game.copy(favorite = false)
+    suspend operator fun invoke(game: Game) = repository.unmarkFavorite(game).each {
+        game.copy(favorite = false).also {
+            state.tryEmit(it)
+        }
     }
 }
