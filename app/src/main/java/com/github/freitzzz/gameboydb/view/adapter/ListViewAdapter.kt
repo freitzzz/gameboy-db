91f7bbe81e08844d2d3cbf1f0744006857569a16
João Freitas <1160907@isep.ipp.pt>
2025-01-23 12:16:07 +0000

    refactor: provide way to create views instead of inflating view by layout id

--- app/src/main/java/com/github/freitzzz/gameboydb/view/adapter/ListViewAdapter.kt
+++ app/src/main/java/com/github/freitzzz/gameboydb/view/adapter/ListViewAdapter.kt
@@ -8,16 +8,26 @@ import android.widget.LinearLayout
 import androidx.annotation.LayoutRes
 
 class ListViewAdapter<T>(
-    @LayoutRes private val itemLayoutId: Int,
+    private val onCreateView: ViewGroup.(Int) -> View,
     private val onBind: View.(data: T) -> Unit,
     private val onLayoutParams: LinearLayout.LayoutParams.() -> Unit = {},
     private val data: MutableList<T> = arrayListOf()
 ) : BaseAdapter() {
+    constructor(
+        @LayoutRes itemLayoutId: Int,
+        onBind: View.(data: T) -> Unit,
+        onLayoutParams: LinearLayout.LayoutParams.() -> Unit = {},
+        data: MutableList<T> = arrayListOf()
+    ) : this(
+        onCreateView = { LayoutInflater.from(this.context).inflate(itemLayoutId, this, false) },
+        onBind = onBind,
+        onLayoutParams = onLayoutParams,
+        data = data
+    )
+
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
         if (convertView == null) {
-            val itemView: View =
-                LayoutInflater.from(parent!!.context).inflate(itemLayoutId, parent, false)
-
+            val itemView = onCreateView(parent!!, position)
             itemView.layoutParams = LinearLayout.LayoutParams(itemView.layoutParams).apply {
                 onLayoutParams()
             }
