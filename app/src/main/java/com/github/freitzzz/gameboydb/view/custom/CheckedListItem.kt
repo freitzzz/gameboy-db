581ec6ee30e7cbce07d25cc4f1a0700075941c8b
João Freitas <1160907@isep.ipp.pt>
2025-01-23 10:14:57 +0000

    refactor: rename to CheckedListItem

--- app/src/main/java/com/github/freitzzz/gameboydb/view/custom/CheckedListItem.kt
+++ app/src/main/java/com/github/freitzzz/gameboydb/view/custom/CheckedListItem.kt
@@ -11,12 +11,12 @@ import com.github.freitzzz.gameboydb.view.show
 import com.github.freitzzz.gameboydb.view.view
 import com.github.freitzzz.gameboydb.view.viewOf
 
-class PreferenceListItem @JvmOverloads constructor(
+class CheckedListItem @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null
 ) : ConstraintLayout(context, attrs) {
     var value: String? = null
         set(value) {
-            viewOf<TextView>(R.id.preference_list_item_value).let {
+            viewOf<TextView>(R.id.checked_list_item_value).let {
                 it.text = value
                 field = value
             }
@@ -24,23 +24,24 @@ class PreferenceListItem @JvmOverloads constructor(
 
     var checked: Boolean = false
         set(value) {
-            view(R.id.preference_list_item_check).let {
+            view(R.id.checked_list_item_check).let {
                 if (value) it.show() else it.remove()
                 field = value
             }
         }
 
     init {
-        val typedArray = context.obtainStyledAttributes(attrs, R.styleable.PreferenceListItem)
-        val value = typedArray.getString(R.styleable.PreferenceListItem_value)
-        val checked = typedArray.getBoolean(R.styleable.PreferenceListItem_checked, false)
+        val typedArray = context.obtainStyledAttributes(attrs, R.styleable.CheckedListItem)
+        val value = typedArray.getString(R.styleable.CheckedListItem_value)
+        val checked = typedArray.getBoolean(R.styleable.CheckedListItem_checked, false)
         typedArray.recycle()
 
-        View.inflate(context, R.layout.layout_preference_list_item, this)
+        View.inflate(context, R.layout.layout_checked_list_item, this)
         this.value = value
         this.checked = checked
+        this.layoutParams = LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT)
 
-        if(isInEditMode){
+        if (isInEditMode) {
             this.value = "Value"
             this.checked = true
         }
